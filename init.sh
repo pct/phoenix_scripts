76bdaebf41e3652673ccb1ab73ad5388b21cf937
Jin-Sih Lin <linpct@gmail.com>
2016-09-22 06:22:48 +0800

    sync init

--- init.sh
+++ init.sh
@@ -9,10 +9,11 @@ PROJECT_NAME=$1
 BASEDIR=$(dirname "$0")
 
 # === install/upgrade hex ===
+echo '=== install/upgrade hex ==='
 mix local.hex
 
 # === init ===
-echo 'create project:' $PROJECT_NAME '...'
+echo '=== create project:' $PROJECT_NAME '==='
 mix phoenix.new $PROJECT_NAME --database mysql
 echo "# Vim\n*~\n*swp" >> $PROJECT_NAME/.gitignore
 
@@ -21,4 +22,10 @@ cd $PROJECT_NAME
 git init; git add .; git commit -m 'init'
 
 # === done ===
-echo "\n\nYou could go 'cd $PROJECT_NAME; mix phoenix.server' now!"
+echo "=== done ==="
+echo "\n\n"
+echo "Projejct '$PROJECT_NAME' created! You could:"
+echo "[1] cd $PROJECT_NAME"
+echo "[2] edit 'config/dev.exs' for development db settings"
+echo "[3] use 'mix ecto.create' to create development db"
+echo "[4] use 'mix phoenix.server' to start phoenix server"
