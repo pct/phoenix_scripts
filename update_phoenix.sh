0af087072512dc776e21f18f6cee9235ac905fd6
Jin-Sih Lin <linpct@gmail.com>
2017-11-18 17:43:10 +0800

    clean 1.2 install

--- update_phoenix.sh
+++ update_phoenix.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-# 1.2
-#mix archive.install https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez
-
 # 1.3
 mix archive.install https://github.com/phoenixframework/archives/raw/master/phx_new.ez
